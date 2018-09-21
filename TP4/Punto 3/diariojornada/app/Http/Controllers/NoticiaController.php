50e2dfd0afb0632d1629a5f887215ddb41e48fe5
ivanjavierparra <ivanjavierparra@hotmail.com>
2018-09-21 14:28:30 -0300

    api terminada

--- TP4/Punto 3/diariojornada/app/Http/Controllers/NoticiaController.php
+++ TP4/Punto 3/diariojornada/app/Http/Controllers/NoticiaController.php
@@ -8,13 +8,17 @@ use Illuminate\Http\Request;
 class NoticiaController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Devuelvo todas las noticias
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+         
+        $noticias = Noticia::all(); 
+        return response()->json(
+             $noticias
+        );
     }
 
     /**
@@ -28,25 +32,52 @@ class NoticiaController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Guardo una noticia.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        
+        $data = $request->all();
+
+        try{
+            $noticia = new Noticia;
+            $noticia->titulo = $data['titulo'];
+            $noticia->subtitulo = $data['subtitulo'];
+            $noticia->fecha_creacion = $data['fecha_creacion'];
+            $noticia->seccion = $data['seccion'];
+            $noticia->visitas = $data['visitas'];
+            $noticia->noticias_relacionadas = $data['noticias_relacionadas'];
+            $noticia->tags = $data['tags'];
+            $noticia->cantidad_comentarios = $data['cantidad_comentarios'];
+            $noticia->cantidad_megusta = $data['cantidad_megusta'];
+            $noticia->primer_parrafo = $data['primer_parrafo'];
+            $noticia->texto = $data['texto'];
+            $noticia->save();
+        }catch(Exception $e){
+            print( $e->getMessage());
+        }
+                
+
+        return response()->json(
+            $noticia
+         );
     }
 
+   
     /**
-     * Display the specified resource.
+     * Muestra datos de una noticia.
      *
      * @param  \App\Noticia  $noticia
      * @return \Illuminate\Http\Response
      */
-    public function show(Noticia $noticia)
+    public function show($id)
     {
-        //
+        return response()->json(
+            Noticia::find($id)
+        );
     }
 
     /**
@@ -61,25 +92,167 @@ class NoticiaController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualiza datos de una noticia.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Noticia  $noticia
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Noticia $noticia)
+    public function update(Request $request, $id)
     {
         //
+        $data = $request->all();
+        $noticia = Noticia::find($id);
+
+        if (!empty($noticia)) {
+            $noticia->titulo = $data['titulo'];
+            #$noticia->subtitulo = $data['subtitulo'];
+            #$noticia->fecha_creacion = $data['fecha_creacion'];
+            $noticia->seccion = $data['seccion'];
+            #$noticia->visitas = $data['visitas'];
+            #$noticia->noticias_relacionadas = $data['noticias_relacionadas'];
+            #$noticia->tags = $data['tags'];
+            #$noticia->cantidad_comentarios = $data['cantidad_comentarios'];
+            #$noticia->cantidad_megusta = $data['cantidad_megusta'];
+            #$noticia->primer_parrafo = $data['primer_parrafo'];
+            #$noticia->texto = $data['texto'];
+            $noticia->update();
+        }
+        return response()->json(
+            $noticia
+        );
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Elimina una noticia.
      *
      * @param  \App\Noticia  $noticia
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Noticia $noticia)
+    public function destroy($id)
     {
-        //
+        $noticia = Noticia::find($id);
+        $resultado = $noticia;
+        if(!empty($noticia)){
+            $noticia->delete();
+        }
+
+        return response()->json(
+            $resultado
+        );
     }
+
+    /**
+     * Devuelve todas las noticias de una Seccion.
+     */
+    public function getNoticiasSeccion($seccion)
+    {
+        $noticias = Noticia::all()->where("seccion",$seccion); 
+        
+        return response()->json(
+            $noticias
+        );
+    }
+
+
+    /**
+     * Devuelve las 10 noticias mas vistas de una seccion.
+     */
+    public function getNoticiasSeccionVisitas($seccion)
+    {
+        $noticias = Noticia::where("seccion", $seccion)
+                    ->orderBy('visitas', 'desc')
+                    ->take(10)
+                    ->get();
+       
+        return response()->json(
+            $noticias
+        );
+    }
+
+
+    /**
+     * Devuelve las 10 noticias mas vistas del diario.
+     */
+    public function getNoticiasTopTen()
+    {
+        $noticias = Noticia::orderBy('visitas', 'desc')
+                    ->take(10)
+                    ->get();
+
+       
+        return response()->json(
+            $noticias
+        );
+    }
+
+    
+    /**
+     * Devuelve una noticia a partir de su titulo.
+     */
+    public function getNoticiasByTitulo($titulo)
+    {
+        $noticias = Noticia::where("titulo", $titulo)->first();
+        return response()->json(
+            $noticias
+        );
+    }
+
+
+    /**
+     * Devuelve los tags de una noticia.
+     */
+    public function getNoticiasTags($id)
+    {
+        $noticia = Noticia::find($id);
+        $tags = "";
+
+        if (!empty($noticia)) {
+            $tags = $noticia->tags;
+        }
+
+        return response()->json(
+            $tags
+        );
+            
+    }
+
+
+    /**
+     * Devuelve las noticias relacionadas a una noticia especifica.
+     */
+    public function getNoticiasRelacionadas($id)
+    {
+        $noticia = Noticia::find($id);
+        $noticias_relacionadas = "";
+
+        if (!empty($noticia)) {
+            $noticias_relacionadas = $noticia->noticias_relacionadas;
+        }
+
+        return response()->json(
+            $noticias_relacionadas
+        );
+    }
+
+
+    /**
+     * Devuelve todo el contenido de una noticia.
+     */
+    public function getNoticiasContenido($id)
+    {
+        $noticia = Noticia::find($id);
+        $texto = "";
+
+        if (!empty($noticia)) {
+            $texto = $noticia->texto;
+        }
+
+        return response()->json(
+            $texto
+        );
+    }
+
+
+
 }
--- TP4/Punto 3/diariojornada/routes/web.php
+++ TP4/Punto 3/diariojornada/routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*     APIS DE NOTICIAS   */
+//crud
+Route::get('api/noticias', 'NoticiaController@index');
+Route::post('api/noticias', 'NoticiaController@store');
+
+Route::get('api/noticias/{id?}', 'NoticiaController@show');
+Route::put('api/noticias/{id?}', 'NoticiaController@update');
+Route::delete('api/noticias/{id?}', 'NoticiaController@destroy');
+
+//filtros sobre seccion
+Route::get('api/noticias/seccion/{seccion?}', 'NoticiaController@getNoticiasSeccion');
+Route::get('api/noticias/seccion/{seccion?}/topten', 'NoticiaController@getNoticiasSeccionVisitas');
+
+// Filtros sobre noticias
+Route::get('api/noticias/{id?}/tags', 'NoticiaController@getNoticiasTags');
+Route::get('api/noticias/{id?}/relacionadas', 'NoticiaController@getNoticiasRelacionadas');
+Route::get('api/noticias/{id?}/contenido', 'NoticiaController@getNoticiasContenido');
+Route::get('api/noticias/topten/historico', 'NoticiaController@getNoticiasTopTen');
+Route::get('api/noticias/titulo/{titulo?}', 'NoticiaController@getNoticiasByTitulo');
